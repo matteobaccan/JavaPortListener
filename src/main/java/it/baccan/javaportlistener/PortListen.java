e366bdede1223cd3db7cd0f1c2974f156df51e9e
Matteo Baccan <matteo.baccan@gmail.com>
2019-12-23 12:29:32 +0100

    Cleanup log, add rDNS from ip

--- src/main/java/it/baccan/javaportlistener/PortListen.java
+++ src/main/java/it/baccan/javaportlistener/PortListen.java
@@ -40,7 +40,7 @@ import org.slf4j.LoggerFactory;
  * @author Matteo Baccan
  */
 public class PortListen extends Thread {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(PortListen.class);
 
     /**
@@ -49,30 +49,30 @@ public class PortListen extends Thread {
      */
     static public void main(String argv[]) {
         PropertyConfigurator.configure("log4j.properties");
-        
+
         LOG.info("+---------------------------------------------------------------------------+");
         LOG.info("| TCP/IP Port Listener                                         Version 1.01 |");
         LOG.info("| by Matteo Baccan                                    https://www.baccan.it |");
         LOG.info("+---------------------------------------------------------------------------+");
         LOG.info("Opening portListen.ini ...");
-        
+
         try (RandomAccessFile portListenerIni = new RandomAccessFile("portListen.ini", "rw")) {
-            
+
             String ip = "0.0.0.0";
             if (argv.length > 0) {
                 ip = argv[0];
             }
-            
+
             String line = portListenerIni.readLine();
-            
+
             while (line != null) {
-                
+
                 StringTokenizer token = new StringTokenizer(line, ",");
-                
+
                 String type = "";
                 String portFrom = "";
                 String portDesc = "";
-                
+
                 if (token.hasMoreTokens()) {
                     type = token.nextToken();
                     type = type.replace("\"", "");
@@ -84,7 +84,7 @@ public class PortListen extends Thread {
                     portDesc = token.nextToken();
                     portDesc = portDesc.replace("\"", "");
                 }
-                
+
                 if ("TCP".equals(type) && portFrom.length() > 0) {
                     PortListen server = new PortListen(ip, Integer.parseInt(portFrom), portDesc);
                     server.start();
@@ -93,16 +93,16 @@ public class PortListen extends Thread {
                 }
                 line = portListenerIni.readLine();
             }
-            
+
             LOG.info("All system ready");
-            
+
         } catch (FileNotFoundException ex) {
             LOG.error("Error during opening of portListen.ini " + ex.getMessage());
         } catch (IOException ex) {
             LOG.error("Error during reading of portListen.ini " + ex.getMessage());
         }
     }
-    
+
     private final int nPortFrom;
     private final String cInServer;
     private final String cPortDesc;
@@ -119,13 +119,14 @@ public class PortListen extends Thread {
         cPortDesc = desc;
         LOG.info(" listen [" + cInServer + ":" + nPortFrom + ":" + cPortDesc + "] ");
     }
-    
+
     @Override
     public void run() {
         try (ServerSocket sock = new ServerSocket(nPortFrom, 5, InetAddress.getByName(cInServer))) {                                  // port, maxrequest, address
             while (true) {
                 try (Socket socket = sock.accept()) {
-                    LOG.info(" New connection from [{}:{} ({})({})] -> [{}:{}} - [{}]", socket.getInetAddress().getHostAddress(), socket.getPort(), socket.getInetAddress().getHostName(), socket.getInetAddress().getCanonicalHostName(), socket.getLocalAddress().getHostAddress(), socket.getLocalPort(), cPortDesc);
+                    String reverseDNS = getHostName(socket.getInetAddress().getHostAddress());
+                    LOG.info(" New connection from [{}:{}] HostName [{}] -> [{}:{}} - [{}]", socket.getInetAddress().getHostAddress(), socket.getPort(), reverseDNS, socket.getLocalAddress().getHostAddress(), socket.getLocalPort(), cPortDesc);
                 }
             }
         } catch (BindException be) {
@@ -134,5 +135,63 @@ public class PortListen extends Thread {
             LOG.error(e.getMessage());
             LOG.error("Error in listening [{}]", nPortFrom);
         }
+    }
+
+    /**
+     * Do a reverse DNS lookup to find the host name associated with an IP
+     * address. Gets results more often than
+     * {@link java.net.InetAddress#getCanonicalHostName()}, but also tries the
+     * Inet implementation if reverse DNS does not work.
+     *
+     * Based on code found at
+     * http://www.codingforums.com/showpost.php?p=892349&postcount=5
+     *
+     * @param ip The IP address to look up
+     * @return The host name, if one could be found, or the IP address
+     */
+    private static String getHostName(final String ip) {
+        String retVal = null;
+        final String[] bytes = ip.split("\\.");
+        if (bytes.length == 4) {
+            try {
+                final java.util.Hashtable<String, String> env = new java.util.Hashtable<String, String>();
+                env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+                final javax.naming.directory.DirContext ctx = new javax.naming.directory.InitialDirContext(env);
+                final String reverseDnsDomain = bytes[3] + "." + bytes[2] + "." + bytes[1] + "." + bytes[0] + ".in-addr.arpa";
+                final javax.naming.directory.Attributes attrs = ctx.getAttributes(reverseDnsDomain, new String[]{
+                    "PTR",});
+                for (final javax.naming.NamingEnumeration<? extends javax.naming.directory.Attribute> ae = attrs.getAll(); ae.hasMoreElements();) {
+                    final javax.naming.directory.Attribute attr = ae.next();
+                    final String attrId = attr.getID();
+                    for (final java.util.Enumeration<?> vals = attr.getAll(); vals.hasMoreElements();) {
+                        String value = vals.nextElement().toString();
+                        // System.out.println(attrId + ": " + value);
+
+                        if ("PTR".equals(attrId)) {
+                            final int len = value.length();
+                            if (value.charAt(len - 1) == '.') {
+                                // Strip out trailing period
+                                value = value.substring(0, len - 1);
+                            }
+                            retVal = value;
+                        }
+                    }
+                }
+                ctx.close();
+            } catch (final javax.naming.NamingException e) {
+                // No reverse DNS that we could find, try with InetAddress
+                System.out.print(""); // NO-OP
+            }
+        }
+
+        if (null == retVal) {
+            try {
+                retVal = java.net.InetAddress.getByName(ip).getCanonicalHostName();
+            } catch (final java.net.UnknownHostException e1) {
+                retVal = ip;
+            }
+        }
+
+        return retVal;
     }
 }
